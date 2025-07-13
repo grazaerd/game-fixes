78457cf4fec753e8dfe7beabb79b5da79c463ca7
dnblank123 <dnblank143@gmail.com>
2025-07-13 13:11:13 +0800

    adjustments

--- DragonNest/Vector3.hpp
+++ DragonNest/Vector3.hpp
@@ -1,6 +1,10 @@
 #ifndef DN_VEC3_HPP
 #define DN_VEC3_HPP
 
+#include <cstdint>
+#include <immintrin.h>
+#include <float.h>
+
 // Better compile with -O3 -ffast-math -mavx
 // else use SSE only to support all CPUs.
 
@@ -12,16 +16,11 @@
 // TODO: Fix MSVC missed branchless optimization on Vec3NormalizeBranchless with SSE
 // TODO: inline assembly?
 
-#include <cstdint>
-#include <immintrin.h>
-#include <float.h>
-
 struct Vec3 { float x, y, z; };
-// do not touch d3dx9 vec3normalize 32bit epsilon //or use 0.000000000000014210855F
-static constexpr float epsilon = FLT_EPSILON; 
-static constexpr float THREE = 3.0f;
-static constexpr const float HALF = 0.5f;
-
+// mimics d3d9x float epsilon
+static constexpr float epsilon = 0.000000000000014210855F; 
+static constexpr float onehalf = 1.5f;
+static constexpr const float half = 0.5f;
 
 // Branchless
 inline Vec3* Vec3NormalizeBranchless(Vec3* out, const Vec3* in) {
@@ -31,12 +30,12 @@ inline Vec3* Vec3NormalizeBranchless(Vec3* out, const Vec3* in) {
 	
 	const float length_sq = x * x + y * y + z * z;
 	
-	const float mask = (length_sq < epsilon) ? 1.0f : 0.0f;
-	
 	const float rsqrt_approx = _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(length_sq)));
 	
-	const float rsqrt_refined_unmasked = rsqrt_approx * (THREE * HALF - HALF * length_sq * rsqrt_approx * rsqrt_approx);
+	const float rsqrt_refined_unmasked = rsqrt_approx * (onehalf - half * length_sq * rsqrt_approx * rsqrt_approx);
 	
+	const float mask = (length_sq < epsilon) ? 1.0f : 0.0f;
+
 	const float rsqrt_refined = rsqrt_refined_unmasked * (1.0f - mask);
 
 	out->x = x * rsqrt_refined;
@@ -63,7 +62,7 @@ inline Vec3* Vec3NormalizeBranch(Vec3* out, const Vec3* in) {
 	
 	const float rsqrt_approx = _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(length_sq)));
 	
-	const float rsqrt_refined = rsqrt_approx * (THREE * HALF - HALF * length_sq * rsqrt_approx * rsqrt_approx);
+	const float rsqrt_refined = rsqrt_approx * (onehalf - half * length_sq * rsqrt_approx * rsqrt_approx);
 	
 	out->x = x * rsqrt_refined;
 	out->y = y * rsqrt_refined;
